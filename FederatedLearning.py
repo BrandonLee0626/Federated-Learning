5d5ea70c9b7ac7dce0f2d19d86a75cf2e6251e1f
Brandon-M-Lee <minjae.py@gmail.com>
2024-09-19 15:13:29 +0900

    download dataset and check it

--- FederatedLearning.py
+++ FederatedLearning.py
@@ -6,3 +6,35 @@ import torch
 import torch.nn
 import torch.functional as F
 from torchvision import transforms, datasets
+
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda')
+else:
+    DEVICE = torch.device('cpu')
+
+print("Using PyTorch version:", torch.__version__,' Device:', DEVICE)
+
+BATCH_SIZE = int(input('BATCH_SIZE: '))
+EPOCHS = int(input('EPOCHS: '))
+
+train_dataset = datasets.MNIST(root = "../data/MNIST",
+                               train = True,
+                               download = True,
+                               transform = transforms.ToTensor())
+
+test_dataset = datasets.MNIST(root = "../data/MNIST",
+                              train = False,
+                              transform = transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                           batch_size = BATCH_SIZE,
+                                           shuffle = True)
+
+test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
+                                          batch_size = BATCH_SIZE,
+                                          shuffle = False)
+
+for (X_train, y_train) in train_loader:
+    print('X_train:', X_train.size(), 'type:', X_train.type())
+    print('y_train:', y_train.size(), 'type:', y_train.type())
+    break
