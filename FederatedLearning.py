f20aae794159f04dcc794d50a4f411e0a9b43055
BrandonLee0626 <leeminjae0626@dgist.ac.kr>
2024-09-27 00:44:01 +0900

    implement FedSGD and FedAvg

--- FederatedLearning.py
+++ FederatedLearning.py
@@ -6,6 +6,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms, datasets
 
+import matplotlib.pyplot as plt
+
 if torch.cuda.is_available():
     DEVICE = torch.device('cuda')
 else:
@@ -14,7 +16,6 @@ else:
 print("Using PyTorch version:", torch.__version__,' Device:', DEVICE)
 
 BATCH_SIZE = int(input('BATCH_SIZE: '))
-EPOCHS = int(input('EPOCHS: '))
 
 train_dataset = datasets.MNIST(root = "../data/MNIST",
                                train = True,
@@ -24,6 +25,10 @@ train_dataset = datasets.MNIST(root = "../data/MNIST",
 test_dataset = datasets.MNIST(root = "../data/MNIST",
                               train = False,
                               transform = transforms.ToTensor())
+
+train_loader = torch.utils.data.DataLoader(dataset = train_dataset,
+                                           batch_size = BATCH_SIZE,
+                                           shuffle = True)
 
 test_loader = torch.utils.data.DataLoader(dataset = test_dataset,
                                           batch_size = BATCH_SIZE,
@@ -36,10 +41,10 @@ def creat_clients(num_clients=10, initial='clients'):
     shards = torch.utils.data.DataLoader(dataset = train_dataset,
                                          batch_size = size,
                                          shuffle = True)
-    
+
     assert(len(shards) == len(client_names))
 
-    return {client_names[i] : data for (i, data) in enumerate(shards)} 
+    return {client_names[i] : data for (i, data) in enumerate(shards)}
 
 clients = creat_clients()
 
@@ -83,13 +88,11 @@ class Net(nn.Module):
         x = self.fc3(x)
         x = F.log_softmax(x, dim=1)
         return x
-    
-global_model = Net().to(DEVICE)
+
 criterion = nn.CrossEntropyLoss()
 
-def train(model, train_loader):
+def train(model, train_loader, optimizer):
     model.train()
-    optimizer = torch.optim.SGD(global_model.parameters(), lr=0.01, momentum=0.5)
     for batch_idx, (image, label) in enumerate(train_loader):
         image = image.to(DEVICE)
         label = label.to(DEVICE)
@@ -99,7 +102,7 @@ def train(model, train_loader):
         loss.backward()
         optimizer.step()
 
-def federated_learning(global_model, comm_rounds=100):
+def FedSGD(global_model, global_optimizer, EPOCHS=10, comm_rounds=10):
     for comm_round in range(comm_rounds):
         global_weight = {'fc1': global_model.fc1.weight.data, 'fc2':global_model.fc2.weight.data, 'fc3': global_model.fc3.weight.data}
         global_bias = {'fc1': global_model.fc1.bias.data, 'fc2': global_model.fc2.bias.data, 'fc3': global_model.fc3.bias.data}
@@ -123,24 +126,87 @@ def federated_learning(global_model, comm_rounds=100):
             local_model.fc3.weight.data = global_weight['fc3']
             local_model.fc3.bias.data = global_bias['fc3']
 
-            train(local_model, clients_batched[client_name])
+            for EPOCH in range(EPOCHS):
+              train(local_model, clients_batched[client_name], global_optimizer)
 
             scaling_factor = weight_scalling_factor(clients_batched, client_name)
 
             scaled_fc1_weight = scale_model_parameter(local_model.fc1.weight.data, scaling_factor)
             scaled_local_fc1_wieght_list.append(scaled_fc1_weight)
-            scaled_fc2_weight = scale_model_parameter(local_model.fc2.weight.data, scaling_factor)
-            scaled_local_fc2_wieght_list.append(scaled_fc2_weight)
-            scaled_fc3_weight = scale_model_parameter(local_model.fc3.weight.data, scaling_factor)
-            scaled_local_fc3_wieght_list.append(scaled_fc3_weight)
-            
             scaled_fc1_bias = scale_model_parameter(local_model.fc1.bias.data, scaling_factor)
             scaled_local_fc1_bias_list.append(scaled_fc1_bias)
+
+            scaled_fc2_weight = scale_model_parameter(local_model.fc2.weight.data, scaling_factor)
+            scaled_local_fc2_wieght_list.append(scaled_fc2_weight)
             scaled_fc2_bias = scale_model_parameter(local_model.fc2.bias.data, scaling_factor)
             scaled_local_fc2_bias_list.append(scaled_fc2_bias)
+
+            scaled_fc3_weight = scale_model_parameter(local_model.fc3.weight.data, scaling_factor)
+            scaled_local_fc3_wieght_list.append(scaled_fc3_weight)
             scaled_fc3_bias = scale_model_parameter(local_model.fc3.bias.data, scaling_factor)
             scaled_local_fc3_bias_list.append(scaled_fc3_bias)
-            
+
+        average_fc1_weight = sum_scaled_weights(scaled_local_fc1_wieght_list)
+        global_model.fc1.weight.data = average_fc1_weight
+        average_fc1_bias = sum_scaled_weights(scaled_local_fc1_bias_list)
+        global_model.fc1.bias.data = average_fc1_bias
+
+        average_fc2_weight = sum_scaled_weights(scaled_local_fc2_wieght_list)
+        global_model.fc2.weight.data = average_fc2_weight
+        average_fc2_bias = sum_scaled_weights(scaled_local_fc2_bias_list)
+        global_model.fc2.bias.data = average_fc2_bias
+
+        average_fc3_weight = sum_scaled_weights(scaled_local_fc3_wieght_list)
+        global_model.fc3.weight.data = average_fc3_weight
+        average_fc3_bias = sum_scaled_weights(scaled_local_fc3_bias_list)
+        global_model.fc3.bias.data = average_fc3_bias
+
+def FedAvg(global_model, global_optimizer, C, EPOCHS=10, comm_rounds=10):
+    for comm_round in range(comm_rounds):
+        global_weight = {'fc1': global_model.fc1.weight.data, 'fc2':global_model.fc2.weight.data, 'fc3': global_model.fc3.weight.data}
+        global_bias = {'fc1': global_model.fc1.bias.data, 'fc2': global_model.fc2.bias.data, 'fc3': global_model.fc3.bias.data}
+
+        scaled_local_fc1_wieght_list = list()
+        scaled_local_fc1_bias_list = list()
+        scaled_local_fc2_wieght_list = list()
+        scaled_local_fc2_bias_list = list()
+        scaled_local_fc3_wieght_list = list()
+        scaled_local_fc3_bias_list = list()
+
+        m = max(C*len(clients_batched), 1)
+
+        client_names = list(clients_batched.keys())
+        client_names = random.sample(client_names, m)
+
+        for client_name in client_names:
+            local_model = Net().to(DEVICE)
+            local_model.fc1.weight.data = global_weight['fc1']
+            local_model.fc1.bias.data = global_bias['fc1']
+            local_model.fc2.weight.data = global_weight['fc2']
+            local_model.fc2.bias.data = global_bias['fc2']
+            local_model.fc3.weight.data = global_weight['fc3']
+            local_model.fc3.bias.data = global_bias['fc3']
+
+            for EPOCH in EPOCHS:
+              train(local_model, clients_batched[client_name], global_optimizer)
+
+            scaling_factor = weight_scalling_factor(clients_batched, client_name)
+
+            scaled_fc1_weight = scale_model_parameter(local_model.fc1.weight.data, scaling_factor)
+            scaled_local_fc1_wieght_list.append(scaled_fc1_weight)
+            scaled_fc1_bias = scale_model_parameter(local_model.fc1.bias.data, scaling_factor)
+            scaled_local_fc1_bias_list.append(scaled_fc1_bias)
+
+            scaled_fc2_weight = scale_model_parameter(local_model.fc2.weight.data, scaling_factor)
+            scaled_local_fc2_wieght_list.append(scaled_fc2_weight)
+            scaled_fc2_bias = scale_model_parameter(local_model.fc2.bias.data, scaling_factor)
+            scaled_local_fc2_bias_list.append(scaled_fc2_bias)
+
+            scaled_fc3_weight = scale_model_parameter(local_model.fc3.weight.data, scaling_factor)
+            scaled_local_fc3_wieght_list.append(scaled_fc3_weight)
+            scaled_fc3_bias = scale_model_parameter(local_model.fc3.bias.data, scaling_factor)
+            scaled_local_fc3_bias_list.append(scaled_fc3_bias)
+
         average_fc1_weight = sum_scaled_weights(scaled_local_fc1_wieght_list)
         global_model.fc1.weight.data = average_fc1_weight
         average_fc1_bias = sum_scaled_weights(scaled_local_fc1_bias_list)
@@ -160,7 +226,7 @@ def evaluate(model, test_loader):
     model.eval()
     test_loss = 0
     correct = 0
-    
+
     with torch.no_grad():
         for image, label in test_loader:
             image = image.to(DEVICE)
@@ -169,7 +235,31 @@ def evaluate(model, test_loader):
             test_loss += criterion(output, label).item()
             prediction = output.max(1, keepdim=True)[1]
             correct += prediction.eq(label.view_as(prediction)).sum().item()
-    
+
     test_loss /= (len(test_loader.dataset)/ BATCH_SIZE)
     test_accuracy = 100. * correct/len(test_loader.dataset)
     return test_loss, test_accuracy
+
+baseline_model = Net().to(DEVICE)
+baseline_optimizer = torch.optim.SGD(baseline_model.parameters(), lr=0.01, momentum=0.5)
+
+baseline_test_accuracies = list()
+
+for comm_rounds in range(0,50, 5):
+  FedSGD(baseline_model, baseline_optimizer, comm_rounds=comm_rounds)
+  test_loss, test_accuracy = evaluate(baseline_model, test_loader)
+  baseline_test_accuracies.append(test_accuracy)
+
+plt.plot(baseline_test_accuracies)
+
+global_model = Net().to(DEVICE)
+global_optimizer = torch.optim.SGD(global_model.parameters(), lr=0.01, momentum=0.5)
+
+global_test_accuracies = list()
+
+for comm_rounds in range(0,50, 5):
+  FedAvg(global_model, global_optimizer, comm_rounds=comm_rounds)
+  test_loss, test_accuracy = evaluate(global_model, test_loader)
+  global_test_accuracies.append(test_accuracy)
+
+plt.plot(global_test_accuracies)
